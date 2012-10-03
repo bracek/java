64565280303f6473308c5555dee75c3ff727a189
Miroslav Katrak <miroslav.katrak@gmail.com>
2012-10-03 14:15:23 +0200

    added finbugs-exclude.xml file

--- android/Alarm/src/com/vodafone/alarm/AbstractAlarmReceiver.java
+++ android/Alarm/src/com/vodafone/alarm/AbstractAlarmReceiver.java
@@ -4,12 +4,7 @@ import android.content.BroadcastReceiver;
 
 public abstract class AbstractAlarmReceiver extends BroadcastReceiver {
 
-	protected static final int VIBRATE_SECOND = 10000;
+	protected static final int VIBRATE_SECOND = 500;
 	protected static final int ALARM_SECOND = 1;
 	protected static final int ALARM_REPETING = 5;
-
-	public AbstractAlarmReceiver() {
-		super();
-	}
-
 }
