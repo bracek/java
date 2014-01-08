9a5519327e2ad9cad81d9bcdee52ee898dd4ed18
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-08 14:25:42 +0100

    adding decorator pattern example nuttydecorator, honeydecorator,

---
+++ books/books/sk-mka-books-patterns/src/main/java/com/javapapers/designpattern/decorator/Icecream.java
@@ -0,0 +1,5 @@
+package com.javapapers.designpattern.decorator;
+
+public interface Icecream {
+	public String makeIcecream();
+}
---
+++ books/books/sk-mka-books-patterns/src/main/java/com/javapapers/designpattern/decorator/SimpleIcecream.java
@@ -0,0 +1,10 @@
+package com.javapapers.designpattern.decorator;
+
+public class SimpleIcecream implements Icecream {
+
+	@Override
+	public String makeIcecream() {
+		return "Base Icecream";
+	}
+
+}
