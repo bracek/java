e5bfaa3089143cd0fff9b99c48c0d636f6e58eb0
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-08 10:09:15 +0100

    alfje

--- jsp/servlet/src/main/java/org/example/HelloServlet.java
+++ jsp/servlet/src/main/java/org/example/HelloServlet.java
@@ -13,7 +13,7 @@ public class HelloServlet extends HttpServlet
   {
     response.setContentType("text/html");
     response.setStatus(HttpServletResponse.SC_OK);
-    response.getWriter().println("<h1>Hello Servlet Mrko a petko</h1>");
+    response.getWriter().println("<h1>Hello Servlet</h1>");
     response.getWriter().println(
         "session=" + request.getSession(true).getId());
   }
--- spring/com.tutorialspoint/src/main/java/com/tutorialspoint/MainApp.java
+++ spring/com.tutorialspoint/src/main/java/com/tutorialspoint/MainApp.java
@@ -4,12 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
-	  public static void main(String[] args) {
-	      ApplicationContext context = 
-	             new ClassPathXmlApplicationContext("Beans.xml");
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Beans.xml");
 
-	      TextEditor te = (TextEditor) context.getBean("textEditor");
+		TextEditor te = (TextEditor) context.getBean("textEditor");
 
-	      te.spellCheck();
-	   }
+		te.spellCheck();
+	}
 }
