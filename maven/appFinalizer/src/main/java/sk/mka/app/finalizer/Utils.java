c7064d4e52bd299ded4bc000bbae9db7487c20db
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-14 16:23:40 +0100

    updated uitls class

--- maven/appFinalizer/src/main/java/sk/mka/app/finalizer/Utils.java
+++ maven/appFinalizer/src/main/java/sk/mka/app/finalizer/Utils.java
@@ -1,18 +1,18 @@
 package sk.mka.app.finalizer;
 
-public class Utils {
+class Utils {
 
 	static String NEW = "new";
-	static final String PROTECTED = "protected";
-	public static final String PRIVATE = "private";
-	public static final String PUBLIC = "public";
-	public static final String ABSTRACT = "abstract";
-	public static final String JAVA = "java";
-	public static final String COMMENT = "//";
-	public static final String LEFT_BRACKET = "(";
-	static final String NEWLINE = "\n";
-	public static final String EMPTY_STRING = "";
-	public static final String PACKAGE = "package";
-	public static final String CLASS = "class";
+	static String PROTECTED = "protected";
+	static String PRIVATE = "private";
+	static String PUBLIC = "public";
+	static String ABSTRACT = "abstract";
+	static String JAVA = "java";
+	static String COMMENT = "//";
+	static String LEFT_BRACKET = "(";
+	static String NEWLINE = "\n";
+	static String EMPTY_STRING = "";
+	static String PACKAGE = "package";
+	static String CLASS = "class";
 
 }
