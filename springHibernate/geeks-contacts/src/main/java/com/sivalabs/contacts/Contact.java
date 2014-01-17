1a5ddaced21147ffd398e1f6da73021b55924730
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-17 13:26:13 +0100

    EI_EXPOSE_REP, Priority: Normal

--- springHibernate/geeks-contacts/src/main/java/com/sivalabs/contacts/Contact.java
+++ springHibernate/geeks-contacts/src/main/java/com/sivalabs/contacts/Contact.java
@@ -13,30 +13,36 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * @author SivaLabs
- *
+ * 
  */
 
 @Entity
-@Table(name="CONTACTS")
-public class Contact
-{
+@Table(name = "CONTACTS")
+public class Contact {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	@Column	private String name;
-	@Column private String address; 
-	@Column private String gender; 
-	@Column private Date dob; 
-	@Column private String email;
-	@Column private String mobile; 
-	@Column private String phone;
-	
-	public Contact()
-	{
+	@Column
+	private String name;
+	@Column
+	private String address;
+	@Column
+	private String gender;
+	@Column
+	private Date dob;
+	@Column
+	private String email;
+	@Column
+	private String mobile;
+	@Column
+	private String phone;
+
+	public Contact() {
 	}
-	
-	public Contact(final int id,final  String name,final  String address,final  String gender,final 			Date dob,final  String email,final  String mobile,final  String phone)
-	{
+
+	public Contact(final int id, final String name, final String address,
+			final String gender, final Date dob, final String email,
+			final String mobile, final String phone) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -47,73 +53,75 @@ public class Contact
 		this.mobile = mobile;
 		this.phone = phone;
 	}
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
 	}
-	public int getId()
-	{
+
+	public int getId() {
 		return id;
 	}
-	public void setId(final int id)
-	{
+
+	public void setId(final int id) {
 		this.id = id;
 	}
-	public String getName()
-	{
+
+	public String getName() {
 		return name;
 	}
-	public void setName(final String name)
-	{
+
+	public void setName(final String name) {
 		this.name = name;
 	}
-	public String getAddress()
-	{
+
+	public String getAddress() {
 		return address;
 	}
-	public void setAddress(final String address)
-	{
+
+	public void setAddress(final String address) {
 		this.address = address;
 	}
-	public String getGender()
-	{
+
+	public String getGender() {
 		return gender;
 	}
-	public void setGender(final String gender)
-	{
+
+	public void setGender(final String gender) {
 		this.gender = gender;
 	}
-	public Date getDob()
-	{
-		return dob;
+
+	public Date getDob() {
+		// return (Date) dob.clone();
+		// return dob;
+		return (Date) dob.clone();
 	}
-	public void setDob(final Date dob)
-	{
+
+	public void setDob(final Date dob) {
 		this.dob = dob;
 	}
-	public String getEmail()
-	{
+
+	public String getEmail() {
 		return email;
 	}
-	public void setEmail(final String email)
-	{
+
+	public void setEmail(final String email) {
 		this.email = email;
 	}
-	public String getMobile()
-	{
+
+	public String getMobile() {
 		return mobile;
 	}
-	public void setMobile(final String mobile)
-	{
+
+	public void setMobile(final String mobile) {
 		this.mobile = mobile;
 	}
-	public String getPhone()
-	{
+
+	public String getPhone() {
 		return phone;
 	}
-	public void setPhone(final String phone)
-	{
+
+	public void setPhone(final String phone) {
 		this.phone = phone;
-	}	
+	}
 }
