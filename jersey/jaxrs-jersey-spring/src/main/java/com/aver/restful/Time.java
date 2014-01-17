f2937e2db5e76877634c38ed53af0b317a2bd040
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-17 10:35:58 +0100

    afafae

--- jersey/jaxrs-jersey-spring/src/main/java/com/aver/restful/Time.java
+++ jersey/jaxrs-jersey-spring/src/main/java/com/aver/restful/Time.java
@@ -11,12 +11,20 @@ public class Time {
 	@XmlElement
 	private String name;
 
-	public void setTime(String time) {
+	public void setTime(final String time) {
 		this.time = time;
 	}
 
-	public void setName(String name) {
+	public void setName(final String name) {
 		this.name = name;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 }
