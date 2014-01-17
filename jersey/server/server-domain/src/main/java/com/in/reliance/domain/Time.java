50468e1f76e82b32b134e7e50a86ffb13fc5df74
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-17 10:34:25 +0100

    xx

--- jersey/server/server-domain/src/main/java/com/in/reliance/domain/Time.java
+++ jersey/server/server-domain/src/main/java/com/in/reliance/domain/Time.java
@@ -4,19 +4,27 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "clock")
-public class Time {
+public final class Time {
 	@XmlElement
 	private String time;
 
 	@XmlElement
 	private String name;
 
-	public void setTime(String time) {
+	public void setTime(final String time) {
 		this.time = time;
 	}
 
-	public void setName(String name) {
+	public void setName(final String name) {
 		this.name = name;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 }
