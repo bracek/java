f37a4802829b87fcf56c450a3dea103319dbba3d
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-21 15:34:16 +0100

    added private only

--- spring/SpringExample/src/main/java/com/mkyong/customer/services/CustomerService.java
+++ spring/SpringExample/src/main/java/com/mkyong/customer/services/CustomerService.java
@@ -6,15 +6,14 @@ import org.springframework.stereotype.Service;
 @Service
 @Scope("singleton")
 //@Scope("prototype")
-public class CustomerService 
-{
-	String message;
-	
-	public String getMessage() {
-		return message;
-	}
+public class CustomerService {
+    private String message;
 
-	public void setMessage(final String message) {
-		this.message = message;
-	}
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
 }
