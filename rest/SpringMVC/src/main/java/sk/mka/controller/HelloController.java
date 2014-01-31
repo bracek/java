4f7a90cdea85e37e75dc4deda48c91a7cdf06dd7
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-31 16:48:14 +0200

    afe

--- rest/SpringMVC/src/main/java/sk/mka/controller/HelloController.java
+++ rest/SpringMVC/src/main/java/sk/mka/controller/HelloController.java
@@ -11,7 +11,6 @@ public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
-
         model.addAttribute("message", "Spring 3 MVC Hello World");
         return "hello";
     }
