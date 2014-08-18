26770ed9a315a1d4b3775c79090dd81cb901dfe5
Miroslav Katrak <miroslav.katrak@gmail.com>
2014-08-18 15:59:30 +0200

    afea

--- primefaces/jtemplate-master/jtemplate-web/src/main/java/com/mm/web/bbean/CardsBeean.java
+++ primefaces/jtemplate-master/jtemplate-web/src/main/java/com/mm/web/bbean/CardsBeean.java
@@ -10,6 +10,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
