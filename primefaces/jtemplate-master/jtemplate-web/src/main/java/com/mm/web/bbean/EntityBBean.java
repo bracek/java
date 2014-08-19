d5a14048671ccffe5a8f83b938ffdec81cbf8218
Miroslav Katrak <miroslav.katrak@gmail.com>
2014-08-19 14:22:05 +0200

    afe

--- primefaces/jtemplate-master/jtemplate-web/src/main/java/com/mm/web/bbean/EntityBBean.java
+++ primefaces/jtemplate-master/jtemplate-web/src/main/java/com/mm/web/bbean/EntityBBean.java
@@ -16,26 +16,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
  * Entity Backed Bean
  *
  * @author Miquel Millan
  * @version 1.0.0
- *
  */
 
 @Named("entityBBean")
 @Scope("session")
 public class EntityBBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Inject
-	private IEntityService entityService;
+    @Inject
+    private IEntityService entityService;
 
-	private int id;
-	private String attribute;
-	private List<Entity> entityList;
+    private int id;
+    private String attribute;
+    private List<Entity> entityList;
 
     public void onCellEdit(CellEditEvent event) {
         Object oldValue = event.getOldValue();
@@ -44,66 +42,62 @@ public class EntityBBean implements Serializable {
         if (newValue != null && !newValue.equals(oldValue)) {
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
             FacesContext.getCurrentInstance().addMessage(null, msg);
-
-//            final Card card = getEntityList().get(event.getRowIndex());
-//            entityService.updateCard(card);
-
         }
     }
 
 
-	public void addEntity() {
-		try {
-			Entity entity = new Entity();
-			entity.setId(getId());
-			entity.setAttribute(getAttribute());
-			getEntityService().addEntity(entity);
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Added!", "Message: "));
+    public void addEntity() {
+        try {
+            Entity entity = new Entity();
+            entity.setId(getId());
+            entity.setAttribute(getAttribute());
+            getEntityService().addEntity(entity);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Added!", "Message: "));
 
-		} catch (DataAccessException e) {
-			e.printStackTrace();
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "D'oh!", "Message: "));
-		}
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "D'oh!", "Message: "));
+        }
 
-	}
+    }
 
 
-	public void reset() {
-		this.setId(0);
-		this.setAttribute("");
-	}
+    public void reset() {
+        this.setId(0);
+        this.setAttribute("");
+    }
 
-	public List<Entity> getEntityList() {
-		entityList = new ArrayList<Entity>();
-		entityList.addAll(getEntityService().getEntitys());
-		return entityList;
-	}
+    public List<Entity> getEntityList() {
+        entityList = new ArrayList<Entity>();
+        entityList.addAll(getEntityService().getEntitys());
+        return entityList;
+    }
 
-	public IEntityService getEntityService() {
-		return entityService;
-	}
+    public IEntityService getEntityService() {
+        return entityService;
+    }
 
-	public void setEntityService(IEntityService entityService) {
-		this.entityService = entityService;
-	}
+    public void setEntityService(IEntityService entityService) {
+        this.entityService = entityService;
+    }
 
-	public void setEntityList(List<Entity> entityList) {
-		this.entityList = entityList;
-	}
+    public void setEntityList(List<Entity> entityList) {
+        this.entityList = entityList;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getAttribute() {
-		return attribute;
-	}
+    public String getAttribute() {
+        return attribute;
+    }
 
-	public void setAttribute(String attribute) {
-		this.attribute = attribute;
-	}
- }
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+}
