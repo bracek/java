2f874f0da06cec159b2a711377bc8ced62a7ea8f
Miroslav Katrak <miroslav.katrak@gmail.com>
2014-09-13 14:52:38 +0200

    afe

--- postgres/jsp/contacts/src/test/java/com/att/uchannels/model/dao/EntityDAOImplTest.java
+++ postgres/jsp/contacts/src/test/java/com/att/uchannels/model/dao/EntityDAOImplTest.java
@@ -1,53 +1,61 @@
-
 package com.att.uchannels.model.dao;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.att.uchannels.AbstractWebTest;
 import com.att.uchannels.TestInterface;
 import com.att.uchannels.domain.Entity;
 
-import javax.annotation.Resource;
-
 /**
  * Created by miroslav.katrak on 4.9.2014.
  */
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "file:src/test/resources/applicationContext.xml" })
 public class EntityDAOImplTest extends AbstractWebTest implements TestInterface {
 
-    @Resource
-    private EntityDAO entityDAO;
+	@Resource
+	private EntityDAO entityDAO;
 
-    @Override
-    public void testInsert() {
-        final Entity obj = new Entity();
-        obj.setAttribute(TEST_ATTRIBUTE);
-        entityDAO.create(obj);
+	@Test
+	@Override
+	public void testInsert() {
+		final Entity obj = new Entity();
+		obj.setAttribute(TEST_ATTRIBUTE);
+		entityDAO.create(obj);
 
-        flush();
-        IdHolder.contactId = obj.getId();
-    }
+		flush();
+		IdHolder.contactId = obj.getId();
+	}
 
-    @Override
-    public void testLoad() {
-        final Entity ent = entityDAO.read(IdHolder.id);
-        assertEquals(TEST_ATTRIBUTE, ent.getAttribute());
-    }
+	@Override
+	public void testLoad() {
+		final Entity ent = entityDAO.read(IdHolder.id);
+		assertEquals(TEST_ATTRIBUTE, ent.getAttribute());
+	}
 
-    @Override
-    public void testUpdate() {
-        Entity obj = entityDAO.read(IdHolder.id);
-        obj.setAttribute(TEST_ATTRIBUTE_UP);
-        entityDAO.update(obj);
-        flush();
+	@Override
+	public void testUpdate() {
+		Entity obj = entityDAO.read(IdHolder.id);
+		obj.setAttribute(TEST_ATTRIBUTE_UP);
+		entityDAO.update(obj);
+		flush();
 
-        obj = entityDAO.read(IdHolder.id);
-        assertEquals(TEST_ATTRIBUTE_UP, obj.getAttribute());
-    }
+		obj = entityDAO.read(IdHolder.id);
+		assertEquals(TEST_ATTRIBUTE_UP, obj.getAttribute());
+	}
 
-    @Override
-    public void testDelete() {
-        Entity obj = entityDAO.read(IdHolder.id);
-        boolean isDeleted = entityDAO.delete(obj);
-        flush();
+	@Override
+	public void testDelete() {
+		Entity obj = entityDAO.read(IdHolder.id);
+		boolean isDeleted = entityDAO.delete(obj);
+		flush();
 
-        assertTrue("User was not successfully deleted", isDeleted);
-    }
+		assertTrue("User was not successfully deleted", isDeleted);
+	}
 }
